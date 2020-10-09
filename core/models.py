3b04772ee8f14234c47925694baf7b999c8e4b0d
AaronOBrien1453 <aatobrien@gmail.com>
2020-10-09 15:33:41 -0400

    initial models created

---
+++ core/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from imagekit.models import ImageSpecField
+from taggit.managers import TaggableManager
+
+from users.models import User
+# Create your models here.
+class Video(models.Model):
+    creator = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='videos')
+    video = models.FileField.whateverThisFieldNeeds
+    thumbnail = models.ImageField(source=VideoOrWhateverTheSourceShouldBe, restofthestuff=restofthestuff)
+    # gallery = models.ForeignKey(to='Gallery', null=True, on_delete=models.CASCADE, related_name='videos')
+    upvoted = models.IntegerField(default=0)
+    tags = TaggableManager()
+    def __str__(self):
+        return self.title
+    
+    
+class Comment(models.Model):
+    text = models.TextField(max_length=5000)
+    pub_date = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='comments')
+    video = models.ForeignKey(to=Video, on_delete=models.CASCADE, related_name='comments')
+    
+# class Gallery(models.Model):
+#     title = models.CharField(max_length=500)
+#     owner = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='galleries', blank=True)
+#     tags = TaggableManager()
+#     def __str__(self):
+#         return self.title
+
--- users/models.py
+++ users/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from imagekit.processors import ResizeToFit
+from imagekit.models import ImageSpecField
 # Consider creating a custom user model from scratch as detailed at
 # https://docs.djangoproject.com/en/3.0/topics/auth/customizing/#specifying-a-custom-user-model
 
 
 class User(AbstractUser):
-    pass
+    is_instructor = models.BooleanField(default=False)
+    profile_photo = models.ImageField(upload_to='profile_photo', null=True)
+    image_medium = ImageSpecField(source='profile_photo',
+                                            processors=[ResizeToFit(300, 300)], format='jpeg', options={'quality': 80})
+    bio = models.TextField(max_length=5000)
+    
