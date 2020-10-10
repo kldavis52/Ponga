07d4c9c1282558e8cc086bc9133e99b1a071d7f9
AaronOBrien1453 <aatobrien@gmail.com>
2020-10-10 18:54:58 -0400

    landing page styling and placeholder info added

--- studiopal/urls.py
+++ studiopal/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.urls import include, path
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('registration.backends.simple.urls')),
+    path('landing_page/', views.landing_page, name='landing_page')
 ]
 
 if settings.DEBUG:
