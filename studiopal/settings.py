a71ab95df1fe3c8247a4cd19c7ef82acc6cbb30a
kldavis52 <kyle.linnard.davis@gmail.com>
2020-10-10 11:23:49 -0400

    updated Pipfile and settings.py for azure deployment

--- studiopal/settings.py
+++ studiopal/settings.py
@@ -149,3 +149,14 @@ INTERNAL_IPS = [
     "127.0.0.1",
     # ...
 ]
+
+DEFAULT_FILE_STORAGE = "backend.custom_azure.AzureMediaStorage"
+STATICFILES_STORAGE = "backend.custom_azure.AzureStaticStorage"
+
+STATIC_LOCATION = "static"
+MEDIA_LOCATION = "media"
+
+AZURE_ACCOUNT_NAME = "studiopal"
+AZURE_CUSTOM_DOMAIN = f"{AZURE_ACCOUNT_NAME}.blob.core.windows.net"
+STATIC_URL = f"https://{AZURE_CUSTOM_DOMAIN}/{STATIC_LOCATION}/"
+MEDIA_URL = f"https://{AZURE_CUSTOM_DOMAIN}/{MEDIA_LOCATION}/"
