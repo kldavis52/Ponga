c9f5cfabceb31b1cc6fe9cdc99319bb2a8c53459
[i2i5] <[i1215@icloud.com]>
2020-10-15 12:54:32 -0400

    add_comments_pk_fixed

--- core/views.py
+++ core/views.py
@@ -21,7 +21,7 @@ def video_upload(request):
 
 def video_detail(request, video_pk):
     video = Video.objects.get(id=video_pk)
-    return render(request, "studiopal/video_detail.html", {"video":video,"video_pk":video_pk})
+    return render(request, "studiopal/video_detail.html", {"video":video,"video_pk":video.pk})
 
 
 def landing_page(request):
