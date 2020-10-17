2682c37221181545c29f48bfc79e6953d10567a3
kldavis52 <kyle.linnard.davis@gmail.com>
2020-10-17 14:59:23 -0400

    updated redirect after uploading videos

--- core/views.py
+++ core/views.py
@@ -14,58 +14,63 @@ def video_upload(request):
             video = form.save(commit=False)
             video.creator = request.user
             video.save()
-            return redirect("video_upload")
+            return redirect("video_detail", video_pk=video.pk)
     form = VideoForm()
     return render(request, "studiopal/video_upload.html", {"form": form})
 
 
 def video_detail(request, video_pk):
     video = Video.objects.get(id=video_pk)
-    return render(request, "studiopal/video_detail.html", {"video":video})
+    return render(request, "studiopal/video_detail.html", {"video": video})
 
 
 def landing_page(request):
     videos = Video.objects.all()
     return render(request, "studiopal/landing_page.html", {"videos": videos})
 
+
 def homepage(request):
     return render(request, "studiopal/homepage.html")
 
 
 def add_comment(request, video_pk):
-    video = get_object_or_404(Video, pk = video_pk)
-    if request.method == 'POST':
-        form =VideoForm()
-        form=CommentsForm(data = request.POST)
-        if form. is_valid():
+    video = get_object_or_404(Video, pk=video_pk)
+    if request.method == "POST":
+        form = VideoForm()
+        form = CommentsForm(data=request.POST)
+        if form.is_valid():
             comments = form.save(commit=False)
             comments.author = request.user
             comments.video = video
             comments.save()
-            return redirect (to='video_detail', video_pk=video_pk)
-    return render (request, "studiopal/video_detail.html", {'form':form, 'video':video})
+            return redirect(to="video_detail", video_pk=video_pk)
+    return render(
+        request, "studiopal/video_detail.html", {"form": form, "video": video}
+    )
 
-def delete_comment (request, comment_pk):
+
+def delete_comment(request, comment_pk):
     comment = get_object_or_404(Comment, pk=comment_pk)
-    if request.method == 'POST':
+    if request.method == "POST":
         comment.delete()
-        return redirect(to = "homepage")
-    return render(request,"studiopal/delete_comment.html", {'comment': comment})
+        return redirect(to="homepage")
+    return render(request, "studiopal/delete_comment.html", {"comment": comment})
 
 
 def add_instructor_info(request, user_pk):
     user = get_object_or_404(User.objects.all(), pk=user_pk)
-    if request.method == 'POST':
+    if request.method == "POST":
         form = InstructorForm(data=request.POST, instance=user, files=request.FILES)
         if form.is_valid():
             user = form.save()
-            return redirect(to='instructor_detail', user_pk=user.pk)
+            return redirect(to="instructor_detail", user_pk=user.pk)
     else:
         form = InstructorForm(instance=user)
-    return render(request, 'studiopal/add_instructor_info.html', {'form': form, 'user': user})
-            
+    return render(
+        request, "studiopal/add_instructor_info.html", {"form": form, "user": user}
+    )
+
 
 def instructor_detail(request, user_pk):
     user = get_object_or_404(User.objects.all(), pk=user_pk)
-    return render(request, 'studiopal/instructor_detail.html', {'user': user})
-
+    return render(request, "studiopal/instructor_detail.html", {"user": user})
