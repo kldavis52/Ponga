9a8f52571ecb958c22c3bd8632ae36dd02310392
kldavis52 <kyle.linnard.davis@gmail.com>
2021-02-09 07:33:17 -0500

    added serializers for Django Models

---
+++ api/serializers.py
@@ -0,0 +1,37 @@
+from django.db.models import fields
+from rest_framework import serializers
+from core.models import Video, Comment
+from users.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            "studio_name",
+            "profile_photo",
+            "user_bio",
+            "paypal_donation_url",
+            "joined_date",
+        )
+
+
+class VideoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Video
+        fields = (
+            "title",
+            "description",
+            "video",
+            "video_thumbnail",
+            "publish_date",
+        )
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = (
+            "text",
+            "pub_date",
+        )
