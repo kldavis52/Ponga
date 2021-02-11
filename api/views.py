c996f4dc5f59806e754b0138767712283bb7fcfb
kldavis52 <kyle.linnard.davis@gmail.com>
2021-02-11 17:31:59 -0500

    added VideoListView for all Video objects

--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import views, viewsets, generics, status
+from rest_framework.parsers import MultiPartParser, FormParser
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
+
+from api.serializers import VideoSerializer, CommentSerializer, UserSerializer
+from core.models import Video, Comment
+from users.models import User
+
+
+class VideoListView(generics.ListAPIView):
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
