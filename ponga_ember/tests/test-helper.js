194e4ace8dfc9524c849aa2f75cfd1707f9854c0
kldavis52 <kyle.linnard.davis@gmail.com>
2021-02-12 12:43:57 -0500

    added Ponga Ember UI app

---
+++ ponga_ember/tests/test-helper.js
@@ -0,0 +1,8 @@
+import Application from 'ponga-ember/app';
+import config from 'ponga-ember/config/environment';
+import { setApplication } from '@ember/test-helpers';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+start();
