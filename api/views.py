bc2407ae463b47b3e4166bb6ef7dd7f4958dcec8
kldavis52 <kyle.linnard.davis@gmail.com>
2021-02-13 09:59:19 -0500

    added endpoints to get, put, delete single videos on Details page

--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+from django.http.response import Http404
 from rest_framework import views, viewsets, generics, status
 from rest_framework.parsers import MultiPartParser, FormParser
 
@@ -12,3 +13,33 @@ from users.models import User
 class VideoListView(generics.ListAPIView):
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
+
+
+class VideoDetailView(views.APIView):
+    """
+    Retrieve, update or delete a video
+    """
+
+    def get_object(self, pk):
+        try:
+            return Video.objects.get(pk=pk)
+        except Video.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        video = self.get_object(pk)
+        serializer = VideoSerializer(video)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        video = self.get_object(pk=pk)
+        serializer = VideoSerializer(video, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        video = self.get_object(pk=pk)
+        video.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
